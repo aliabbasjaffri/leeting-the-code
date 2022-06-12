9ae86870d2193ded55b7d7efa8d8f149ef6030a9
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-06-12 12:55:24 +0200

    Added LeetCode-271

---
+++ arrays_hashing/271-encode_decode-strings.py
@@ -0,0 +1,17 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return "<---MESSAME--->".join(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split("<---MESSAME--->")
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
