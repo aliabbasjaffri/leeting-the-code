71dee704986ebdb186aa3ab4060caf529d669928
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-06-12 16:27:03 +0200

    Added solution for kth largest element

---
+++ heap/703-kth-largest-element.py
@@ -0,0 +1,27 @@
+"""
+finding kth largest element in the list
+leetcode url: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+- not attempted by me, followed solution code on neetcode
+"""
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        
+        return self.minHeap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
