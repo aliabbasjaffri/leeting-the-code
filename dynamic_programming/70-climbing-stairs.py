e7710d1bcda669062a0597e9d80cc71f96e1c8b2
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-06-16 11:28:06 +0200

    Added solution for climbing stairs

---
+++ dynamic_programming/70-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        steps = {}
+        def stair_steps(i: int, n: int) -> int:
+            if i > n:
+                return 0
+            elif i == n:
+                return 1
+            elif i in steps and steps[i] > 0:
+                return steps[i]
+            else:
+                steps[i] = stair_steps(i+1, n) + stair_steps(i+2, n)
+                return steps[i]
+        return stair_steps(0, n)
